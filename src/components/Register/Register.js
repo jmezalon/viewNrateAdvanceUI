bc01db61e2f418b817c39e72a358fd51e196ec92
Max Mezalon <jmezalon@gmail.com>
2024-01-02 10:46:58 -0500

    created our first custome hooks

---
+++ src/components/Register/Register.js
@@ -0,0 +1,89 @@
+import "./Register.css";
+import { Link } from "react-router-dom";
+import { useRegistrationform } from "../../hooks/useRegistrationForm";
+
+export default function Register({ user, setUser }) {
+  const { form, errors, isProcessing, handleOnInputChange, handleOnSubmit } =
+    useRegistrationform({ user, setUser });
+
+  return (
+    <div className="Signup">
+      <div className="card">
+        <h2>Create Account</h2>
+
+        {errors.form && <span className="error">{errors.form}</span>}
+        <br />
+
+        <div className="form">
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter a valid email"
+              value={form.email}
+              onChange={handleOnInputChange}
+            />
+            {errors.email && <span className="error">{errors.email}</span>}
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="username">username</label>
+            <input
+              type="test"
+              name="username"
+              placeholder="your_username"
+              value={form.username}
+              onChange={handleOnInputChange}
+            />
+            {errors.username && (
+              <span className="error">{errors.username}</span>
+            )}
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter a secure password"
+              value={form.password}
+              onChange={handleOnInputChange}
+            />
+            {errors.password && (
+              <span className="error">{errors.password}</span>
+            )}
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="passwordConfirm">Confirm Password</label>
+            <input
+              type="password"
+              name="passwordConfirm"
+              placeholder="Confirm your password"
+              value={form.passwordConfirm}
+              onChange={handleOnInputChange}
+            />
+            {errors.passwordConfirm && (
+              <span className="error">{errors.passwordConfirm}</span>
+            )}
+          </div>
+
+          <button
+            className="btn"
+            disabled={isProcessing}
+            onClick={handleOnSubmit}
+          >
+            {isProcessing ? "Loading..." : "Create Account"}
+          </button>
+        </div>
+
+        <div className="footer">
+          <p>
+            Already have an account? Login <Link to="/login">here</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
